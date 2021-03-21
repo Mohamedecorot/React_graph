8ddd79b096349794ff0392d9f5f282df21e06261
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-21 07:23:12 +0100

    Composition et Heritage

--- app.js
+++ app.js
@@ -31,6 +31,28 @@ function tryConvert(temperature, convert) {
 //   document.querySelector("#app")
 // );
 
+function Button({ type, children }) {
+  const className = "btn btn-" + type;
+  return <button className={className}>{children}</button>;
+}
+
+function PrimaryButton({ children }) {
+  return <Button type="primary">{children}</Button>;
+}
+
+function SecondaryButton({ children }) {
+  return <Button type="secondary">{children}</Button>;
+}
+
+function Column2({ left, right }) {
+  return (
+    <div className="row">
+      <div className="col-md-6">{left}</div>
+      <div className="col-md-6">{right}</div>
+    </div>
+  );
+}
+
 class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
@@ -103,17 +125,25 @@ class Calculator extends React.Component {
             onChange={this.handleChange}
           />
         </div> */}
-        <TemperatureInput
-          scale="c"
-          temperature={celsius}
-          onTemperatureChange={this.handleCelsiusChange}
-        />
-        <TemperatureInput
-          scale="f"
-          temperature={fahrenheit}
-          onTemperatureChange={this.handleFahrenheitChange}
+        <Column2
+          left={
+            <TemperatureInput
+              scale="c"
+              temperature={celsius}
+              onTemperatureChange={this.handleCelsiusChange}
+            />
+          }
+          right={
+            <TemperatureInput
+              scale="f"
+              temperature={fahrenheit}
+              onTemperatureChange={this.handleFahrenheitChange}
+            />
+          }
         />
         <BoilingVerdict celsius={celsius} />
+        <PrimaryButton>Envoyer</PrimaryButton>
+        <SecondaryButton>Envoyer</SecondaryButton>
       </div>
     );
   }
