8bf306a8a225bdc42abd6c81c083c4cb71919f64
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-21 08:17:55 +0100

    TP : Liste de produit: affichage des produits

--- app.js
+++ app.js
@@ -1,151 +1,81 @@
-const scaleNames = {
-  c: "Celsius",
-  f: "Fahrenheit",
-};
+const PRODUCTS = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball",
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch",
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5",
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
+];
 
-function toCelsius(fahrenheit) {
-  return ((fahrenheit - 32) * 5) / 9;
+function ProductRow ({product}) {
+    const name = product.stocked ? product.name : <span className="text-danger">{product.name}</span>
+    return <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+    </tr>
 }
 
-function toFahrenheit(celsius) {
-  return (celsius * 9) / 5 + 32;
+function ProductCategoryRow ({category}) {
+    return <tr>
+        <th colSpan="2">{category}</th>
+    </tr>
 }
 
-function BoilingVerdict({ celsius }) {
-  if (celsius >= 100) {
-    return <div className="alert alert-success">L'eau Bout</div>;
-  }
-  return <div className="alert alert-info">L'eau ne bout pas</div>;
+function ProductTable ({products}) {
+    const rows = []
+    let lastCategory = null
+
+    products.forEach(product => {
+        if (product.category !== lastCategory) {
+            lastCategory = product.category
+            rows.push(<ProductCategoryRow key={lastCategory} category={lastCategory}/>)
+        }
+        rows.push(<ProductRow key={product.name} product={product}/>)
+    })
+    return <table className="table">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prix</th>
+            </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+    </table>
 }
 
-function tryConvert(temperature, convert) {
-  const value = parseFloat(temperature);
-  if (Number.isNaN(value)) {
-    return "";
-  }
-  return (Math.round(convert(value) * 100) / 100).toString();
-}
-
-// ReactDOM.render(
-//   <BoilingVerdict celsius={110} />,
-//   document.querySelector("#app")
-// );
-
-function Button({ type, children }) {
-  const className = "btn btn-" + type;
-  return <button className={className}>{children}</button>;
-}
-
-function PrimaryButton({ children }) {
-  return <Button type="primary">{children}</Button>;
-}
-
-function SecondaryButton({ children }) {
-  return <Button type="secondary">{children}</Button>;
-}
-
-function Column2({ left, right }) {
-  return (
-    <div className="row">
-      <div className="col-md-6">{left}</div>
-      <div className="col-md-6">{right}</div>
-    </div>
-  );
-}
-
-class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);
-  }
-
+class FilterableProductTable extends React.Component {
   render() {
-    const { temperature } = this.props;
-    const name = "scale" + this.props.scale;
-    const scaleName = scaleNames[this.props.scale];
-    return (
-      <div className="form-group">
-        <label htmlFor={name}>Température (en {scaleName})</label>
-        <input
-          type="text"
-          id={name}
-          className="form-control"
-          value={temperature}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
+    const { products } = this.props;
+    return <ProductTable products={products}/>
   }
 }
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      scale: "c",
-      temperature: 20,
-    };
-    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-  }
-
-  handleCelsiusChange(temperature) {
-    this.setState({
-      scale: "c",
-      temperature,
-    });
-  }
-
-  handleFahrenheitChange(temperature) {
-    this.setState({
-      scale: "f",
-      temperature,
-    });
-  }
-
-  render() {
-    const { temperature, scale } = this.state;
-    const celsius =
-      scale === "c" ? temperature : tryConvert(temperature, toCelsius);
-    const fahrenheit =
-      scale === "f" ? temperature : tryConvert(temperature, toFahrenheit);
-    return (
-      <div>
-        {/* <div className="form-group">
-          <label htmlFor="celsius">Température (en Celsius)</label>
-          <input
-            type="text"
-            id="celcius"
-            className="form-control"
-            value={temperature}
-            onChange={this.handleChange}
-          />
-        </div> */}
-        <Column2
-          left={
-            <TemperatureInput
-              scale="c"
-              temperature={celsius}
-              onTemperatureChange={this.handleCelsiusChange}
-            />
-          }
-          right={
-            <TemperatureInput
-              scale="f"
-              temperature={fahrenheit}
-              onTemperatureChange={this.handleFahrenheitChange}
-            />
-          }
-        />
-        <BoilingVerdict celsius={celsius} />
-        <PrimaryButton>Envoyer</PrimaryButton>
-        <SecondaryButton>Envoyer</SecondaryButton>
-      </div>
-    );
-  }
-}
-ReactDOM.render(<Calculator />, document.querySelector("#app"));
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById("app")
+);
