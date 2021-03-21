37192fd4631ea387af018148aa9403311afcc00c
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-21 07:22:34 +0100

    Tp convertisseur celsiusfahrenheit

--- app.js
+++ app.js
@@ -1,102 +1,121 @@
-class Home extends React.Component {
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
+
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function BoilingVerdict({ celsius }) {
+  if (celsius >= 100) {
+    return <div className="alert alert-success">L'eau Bout</div>;
+  }
+  return <div className="alert alert-info">L'eau ne bout pas</div>;
+}
+
+function tryConvert(temperature, convert) {
+  const value = parseFloat(temperature);
+  if (Number.isNaN(value)) {
+    return "";
+  }
+  return (Math.round(convert(value) * 100) / 100).toString();
+}
+
+// ReactDOM.render(
+//   <BoilingVerdict celsius={110} />,
+//   document.querySelector("#app")
+// );
+
+class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      nom: "",
-      prenom: "",
-      newsletter: false,
-      checked: true,
-    };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  //   handleChange(e) {
-  //     this.setState({
-  //       nom: e.target.value,
-  //       checked: e.target.checked,
-  //     });
-  //   }
-
-  //   render() {
-  //     return (
-  //       <div>
-  //         <label htmlFor="nom">Mon nom</label>
-  //         <input
-  //           type="text"
-  //           id="nom"
-  //           name="nom"
-  //           value={this.state.nom}
-  //           onChange={this.handleChange}
-  //         />
-  //         <textarea
-  //           type="text"
-  //           id="nom"
-  //           name="nom"
-  //           value={this.state.nom}
-  //           onChange={this.handleChange}
-  //         ></textarea>
-  //         <select value={this.state.nom} onChange={this.handleChange}>
-  //           <option value="demo1">Demo 1</option>
-  //           <option value="demo2">Demo 2</option>
-  //           <option value="demo3">Demo 3</option>
-  //         </select>
-  //         <input
-  //           type="checkbox"
-  //           checked={this.state.checked}
-  //           onChange={this.handleChange}
-  //         />
-  //         {this.state.checked ? (
-  //           <div>un message affiché si on coche la checkbox</div>
-  //         ) : null}
-  //       </div>
-  //     );
-  //   }
-
   handleChange(e) {
-    const name = e.target.name;
-    const type = e.target.type;
-    const value = type === "checkbox" ? e.target.checked : e.target.value;
-    this.setState({
-      [name]: value,
-    });
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
+    const { temperature } = this.props;
+    const name = "scale" + this.props.scale;
+    const scaleName = scaleNames[this.props.scale];
     return (
-      <div>
-        <div>
-          <label htmlFor="nom">Nom</label>
-          <input
-            type="text"
-            value={this.state.nom}
-            onChange={this.handleChange}
-            id="nom"
-            name="nom"
-          />
-        </div>
-        <div>
-          <label htmlFor="prenom">Prenom</label>
-          <input
-            type="text"
-            value={this.state.prenom}
-            onChange={this.handleChange}
-            id="prenom"
-            name="prenom"
-          />
-        </div>
-        <div>
-          <label htmlFor="newsletter">s'abonner à la newsletter</label>
-          <input
-            type="checkbox"
-            checked={this.state.newsletter}
-            onChange={this.handleChange}
-            id="newsletter"
-            name="newsletter"
-          />
-        </div>
+      <div className="form-group">
+        <label htmlFor={name}>Température (en {scaleName})</label>
+        <input
+          type="text"
+          id={name}
+          className="form-control"
+          value={temperature}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
 }
 
-ReactDOM.render(<Home />, document.querySelector("#app"));
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: "c",
+      temperature: 20,
+    };
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+  }
+
+  handleCelsiusChange(temperature) {
+    this.setState({
+      scale: "c",
+      temperature,
+    });
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({
+      scale: "f",
+      temperature,
+    });
+  }
+
+  render() {
+    const { temperature, scale } = this.state;
+    const celsius =
+      scale === "c" ? temperature : tryConvert(temperature, toCelsius);
+    const fahrenheit =
+      scale === "f" ? temperature : tryConvert(temperature, toFahrenheit);
+    return (
+      <div>
+        {/* <div className="form-group">
+          <label htmlFor="celsius">Température (en Celsius)</label>
+          <input
+            type="text"
+            id="celcius"
+            className="form-control"
+            value={temperature}
+            onChange={this.handleChange}
+          />
+        </div> */}
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celsius={celsius} />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Calculator />, document.querySelector("#app"));
